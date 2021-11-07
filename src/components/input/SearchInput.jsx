6d4234f385a160f9294f5e7eddf47443df90ba58
Danilo <didoherculano@gmail.com>
2021-11-07 01:48:47 -0300

    searchinput adicionado

---
+++ src/components/input/SearchInput.jsx
@@ -0,0 +1,49 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
+import './searchinput.scss'
+
+function SearchInput(props) {
+    const [inputV, setinputV] = useState('')
+    const [placeList, setPlaceList] = useState(['Campina','Patos','Joao Pessoa'])
+
+    useEffect(() => {
+        const search = { search: inputV }
+        axios.post('http://localhost:3001/autocomplete', search)
+            .then(resp => {
+                const predictions = resp.data.predictions
+                const newList = []
+                try{
+                    for(var i = 0; i < predictions.length; i++){
+                        newList.push(predictions[i].description)
+                    }
+                }
+                catch{
+                    setPlaceList([])
+                }
+                setPlaceList(newList)
+            
+            })
+    }, [inputV]);
+
+    function renderOption(){
+        return ( placeList.map(place => {
+            return <option value={place}/>
+        }))
+    }
+
+    return ( 
+    <div className="searchinput">
+        <input 
+            type="text"
+            value={inputV}
+            onChange={e=>(setinputV(e.target.value))}
+            list="places" 
+        />
+
+        <datalist id="places">
+            {renderOption()}
+        </datalist>
+    </div> );
+}
+
+export default SearchInput;
--- src/components/main/App.jsx
+++ src/components/main/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './App.scss';
 import Map from '../maps/Maps'
+import SearchInput from '../input/SearchInput';
+import axios from 'axios'
+
 function App() {
+  function search(){
+    console.log('Procurar')
+  }
   return (
     <div className="App">
+      <div className="maincontainer">
+        <SearchInput></SearchInput>
+        <button onClick={e=>search()}>Pesquisar</button>
+      </div>
       <Map></Map>
     </div>
   );
