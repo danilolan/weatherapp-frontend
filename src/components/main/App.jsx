1bf585763f752afdc39146a0f932e96a8f9906d3
Danilo <didoherculano@gmail.com>
2021-11-07 16:37:19 -0300

    getWeather adicionado

--- src/components/main/App.jsx
+++ src/components/main/App.jsx
@@ -1,15 +1,28 @@
 import React,{useState} from 'react'
-import './App.scss';
+import axios from 'axios'
 import Map from '../maps/Maps'
 import Search from '../search/Search';
-import axios from 'axios'
+import './App.scss';
 
 function App() {
   const [location, setLocation] = useState({lat:-7.2206167, lng: -35.8888328})
+  const [weather, setWeather] = useState({})
 
   function getData(data){
     setLocation(data.result.geometry.location)
-    console.log(data)
+    getWeather(data.result.geometry.location)
+  }
+
+  function getWeather(location){
+    axios.get(`http://localhost:3001/weather?lat=${location.lat}&lng=${location.lng}`)
+      .then(resp=>{
+        try{
+          setWeather(resp.data)
+        }
+        catch(error){
+          console.log(error.data)
+        }
+      })
   }
   
   return (
