64e3f5e8f9ac918425d13d2c91419205a44924e6
Danilo <didoherculano@gmail.com>
2021-11-07 00:09:24 -0300

    integração com api do google adicionada

--- src/components/main/App.jsx
+++ src/components/main/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './App.scss';
-
+import Map from '../maps/Maps'
 function App() {
   return (
     <div className="App">
-      Funcionando
+      <Map></Map>
     </div>
   );
 }
---
+++ src/components/maps/Maps.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+import './map.scss'
+
+const containerStyle = {
+  width: '100%',
+  height: '100%'
+};
+
+const center = {
+  lat: 0,
+  lng: 0
+};
+
+function MyComponent() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "***REMOVED***"
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+    <div className="map">
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={1}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+    </div>
+  ) : <></>
+}
+
+export default React.memo(MyComponent)
