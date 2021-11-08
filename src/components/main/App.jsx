b86bdf29e520f8410a74fec768e1f9d2e494384d
Danilo <didoherculano@gmail.com>
2021-11-08 16:49:21 -0300

    requisição inicial do weather adicionado

--- src/components/main/App.jsx
+++ src/components/main/App.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import axios from 'axios'
 
 import './App.scss';
@@ -6,47 +6,47 @@ import 'font-awesome/css/font-awesome.min.css'
 
 import Map from '../maps/Maps'
 import Search from '../search/Search';
+import WeatherContainer from '../weathercontainer/WeatherContainer';
 import switchBackground from '../switchBackground/switchBackground';
 
 
 function App() {
   const [location, setLocation] = useState({lat:-7.2206167, lng: -35.8888328})
-  const [weather, setWeather] = useState({})
-
   const [classes, setClasses] = useState('app cloudy')
-
-
-
-  function getData(data){
-    setLocation(data.result.geometry.location)
-    getWeather(data.result.geometry.location)
-  }
-
-  function getWeather(location){
+  const [weather, setWeather] = useState(()=>{
     axios.get(`http://localhost:3001/weather?lat=${location.lat}&lng=${location.lng}`)
       .then(resp=>{
         try{
-          setWeather(resp.data)
-          
           setClasses('app ' + switchBackground(resp.data.current.condition.code))
+          return resp.data
         }
         catch(error){
           console.log(error)
         }
       })
-  }
+  })
   
-  
+
+  useEffect(() => {
+    axios.get(`http://localhost:3001/weather?lat=${location.lat}&lng=${location.lng}`)
+      .then(resp=>{
+        try{
+          setClasses('app ' + switchBackground(resp.data.current.condition.code))
+          setWeather(resp.data)
+        }
+        catch(error){
+          console.log(error)
+        }
+      })
+  }, [location])
 
   return (
     <div className={classes}>
 
-      <Search getData={e=>getData(e)}/>
+      <Search getData={e=>setLocation(e)}/>
 
       <div className="maincontainer">
-        <div className="weathercontainer">    
-          weather   
-        </div>
+        <WeatherContainer currentWeather={weather}/>
         <div className="statscontainer">
           stats
         </div>
